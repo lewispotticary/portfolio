767b5d31de58b4c15bb081b170df4a4c979dc7bf
Lewis Potticary <lewispotticary@gmail.com>
2021-11-06 17:53:35 +0000

    Added routing and styled drawer

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -3,84 +3,81 @@ import React from 'react'
 //Import style sheet
 import './Navigation.css'
 
-//Import Logo image
-import logo from './Logo.png'
-
 //Import of Material UI components
 import {Breadcrumbs, Link, Typography} from '@mui/material';
 
+import { HashLink } from 'react-router-hash-link';
+
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+
+import { BrowserRouter } from 'react-router-dom';
 
 function Navigation() {
 
     const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-      });
+      right: false,
+    });
 
     const toggleDrawer = (anchor, open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-          return;
-        }
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+      setState({ ...state, [anchor]: open });
+    };
     
-        setState({ ...state, [anchor]: open });
-      };
-    
-      const list = (anchor) => (
-        <Box
-          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-          role="presentation"
-          onClick={toggleDrawer(anchor, false)}
-          onKeyDown={toggleDrawer(anchor, false)}
-        >
-          <List>
-            {['Projects', 'About', 'Contact'].map((text, index) => (
-              <ListItem button key={text}>
-                
-                <ListItemText primary={text} />
-              </ListItem>
+    const list = (anchor) => (
+      <Box
+        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250}}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
+          {['Projects', 'About', 'Contact'].map((text, index) => (
+            <div className="drawerContent">
+              <Link underline="none" color="white" variant="h6" href="/"><b><HashLink smooth className="navLinks" to={"#" + text}>{text}</HashLink></b></Link>
+            </div>
             ))}
-          </List>
-        </Box>
-      );
+        </List> 
+      </Box>
+    );
 
     const logo = "{LP}"
 
     return (
-        <div className="navigationContainer">
-            <div className="navigationContent">
-                <Typography variant="h2" color="white"><b>{logo}</b></Typography>
-                <Breadcrumbs className="breadcrumbMenu" aria-label="breadcrumb" color="white" >
-                    <Link underline="none" color="white" variant="h6" href="/"><b>Projects</b></Link>
+        <BrowserRouter>
+          <div className="navigationContainer">
+              <div className="navigationContent">
+                  <Typography variant="h2" color="white"><b>{logo}</b></Typography>
+                  <Breadcrumbs className="breadcrumbMenu" aria-label="breadcrumb" color="white" >
+                    <Link underline="none" color="white" variant="h6" href="/"><b><HashLink smooth className="navLinks" to="#Projects">Projects</HashLink></b></Link>
                     <Link underline="none" color="white" variant="h6" href=""><b>About</b></Link>
-                    <Link underline="none" color="white" variant="h6" href=""><b>Contact</b></Link>
-                </Breadcrumbs>
-                {['right'].map((anchor) => (
-                <React.Fragment key={anchor}>
-                <MenuIcon style={{fill: "white", fontSize: 50}}  onClick={toggleDrawer(anchor, true)}/>
-                <Drawer
+                    <Link underline="none" color="white" variant="h6" href=""><b><HashLink smooth className="navLinks" to="#Contact">Contact</HashLink></b></Link>
+                  </Breadcrumbs>
+                  {['right'].map((anchor) => (
+                  <React.Fragment key={anchor}>
+                  <MenuIcon style={{fill: "white", fontSize: 50}}  onClick={toggleDrawer(anchor, true)}/>
+                  <Drawer
                     anchor={anchor}
                     open={state[anchor]}
                     onClose={toggleDrawer(anchor, false)}
-                >
+                    PaperProps={{
+                      sx: {
+                        backgroundColor: "black",
+                        color: "white",
+                      }
+                    }}
+                  >
                     {list(anchor)}
-                </Drawer>
-                </React.Fragment>
-            ))}
-            </div>
-        </div>
+                  </Drawer>
+                  </React.Fragment>
+              ))}
+              </div>
+          </div>
+        </BrowserRouter>
     )
 }
 
