2af7d987da390bb4de514c633fa3b7d10b8cad7d
Lewis Potticary <lewispotticary@gmail.com>
2021-11-07 14:52:53 +0000

    Added credits to photo and routing

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 
-import bannerImage from './bannerImage.png'
-
 import './Header.css';
 
-import {Typography, Button, Fab} from '@mui/material';
+import {Typography, Button, Fab, Slide} from '@mui/material';
 
-import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import { HashLink } from 'react-router-hash-link';
+
+import { BrowserRouter } from 'react-router-dom';
 
 function Header() {
     return (
-        <div className="headerContainer">
-            <div className="headerContent">
-                <Typography variant="h2" color="white"><b>Hi! I'm Lewis Potticary</b></Typography>
-                <div className="box">
-                    <Typography variant="h4" color="white"><b>Aspiring Front-end Developer</b></Typography>
-                    <Typography className="boxText" variant="h4" color="white"><b>&nbsp;and Surfer &#10132;</b></Typography>
+        <BrowserRouter>
+            <div className="headerContainer">
+                <div className="headerContent">
+                    <Typography variant="h2" color="white"><b>Hi! I'm Lewis Potticary</b></Typography>
+                    <div className="box">
+                        <Typography variant="h4" color="white"><b>Aspiring Front-end Developer</b></Typography>
+                        <Typography className="boxText" variant="h4" color="white"><b>&nbsp;and Surfer &#10132;</b></Typography>
+                    </div>
+                    <div className="secondaryBox">
+                        <Typography color="white">Picture By: <a href="https://www.elliscollinsphoto.co.uk/" target="_blank">Ellis Collins</a></Typography>
+                    </div>
+                    <HashLink smooth className="navLinks" to="#Projects">
+                        <Button className="projectButton" variant="contained">View Projects</Button>
+                    </HashLink>
                 </div>
-                <Button className="projectButton" variant="contained">View Projects</Button>
             </div>
-        </div>
+        </BrowserRouter>
     )
 }
 
