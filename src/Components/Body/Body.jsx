4ed3e6b66e9e7917f902a80beadc48a71e21f449
Lewis Potticary <lewispotticary@gmail.com>
2021-11-09 22:26:02 +0000

    Added new card to body and styled card rows

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -6,9 +6,11 @@ import covidTrackerImage from './covidTrackerProject.JPG'
 
 import expenseTrackerImage from './expenseTrackerProject.JPG'
 
-import {Typography, Button, Fab, Card, CardMedia, CardContent, CardActions, Grid, Modal, Box, } from '@mui/material';
+import portfolioProject from './portfolioProject.JPG'
 
+import githubImage from './githubImage.png'
 
+import {Typography, Button, Card, CardMedia, CardContent, CardActions, Grid, Modal, Box, } from '@mui/material';
 
 function Body() {
 
@@ -30,28 +32,25 @@ function Body() {
     
     return (
         <div className="bodyContainer">
-            <div className="titleContainer">
+            <div className="titleContainer" id="Projects">
                 <Typography variant="h2" color="white"><b>Projects</b></Typography>
             </div>
             <div className="cardContainer">
                 <Grid container justify="center">
-                    <Grid item component={Card} xs={12} md={5.52} className="card">
+                    <Grid item component={Card} xs={12} md={3.5} className="card">
                         <CardMedia
                         component="img"
                         image={covidTrackerImage}
                         alt="green iguana"
+                        className="cardRow1"
                         />
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="div">Covid-19 Tracker</Typography>
-                            <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
+                            <Typography variant="body1" color="black">
+                            Covid-19 tracker web application. Built using React.JS, Material UI, Chart.JS, and Leaflet.JS. Fully responsive.
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <Box mr={1}>
-                            <Button size="small" onClick={handleOpen} variant="contained" color="success">Full Description</Button>
-                            </Box>
                             <Modal
                             open={open}
                             onClose={handleClose}
@@ -67,45 +66,69 @@ function Body() {
                                 </Typography>
                             </Box>
                             </Modal>
+                            <Box mr={1}>
+                                <Button size="small" onClick={handleOpen} variant="contained" color="success">Full Description</Button>
+                            </Box>
                             <Button size="small" variant="contained" href="https://musing-bassi-1d4cea.netlify.app/" target="_blank">Live Site</Button>
                             <Button size="small" variant="outlined" href="https://github.com/lewispotticary/coronavirus-tracker" target="_blank">Source</Button>
                         </CardActions>
                     </Grid>
-                    <Grid item component={Card} xs={12} md={5.52} className="card">
+                    <Grid item component={Card} xs={12} md={3.5} className="card">
                         <CardMedia
                         component="img"
                         image={expenseTrackerImage}
                         alt="green iguana"
+                        className="cardRow1"
                         />
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="div">Expense Tracker</Typography>
-                            <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
+                            <Typography variant="body1" color="black">
+                            Expense tracker web application. Built using React.JS, Bootstrap and Chart.JS. Fully responsive.
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
+                            <Box mr={1}>
+                                <Button size="small" onClick={handleOpen} variant="contained" color="success">Full Description</Button>
+                            </Box>
+                            <Button size="small" variant="contained" href="https://naughty-montalcini-8be930.netlify.app/" target="_blank">Live Site</Button>
+                            <Button size="small" variant="outlined" href="https://github.com/lewispotticary/react-expense-tracker" target="_blank">Source</Button>
                         </CardActions>
                     </Grid>
-                    <Grid item component={Card} xs={12} md={5.52} className="card">
+                    <Grid item component={Card} xs={12} md={3.5} className="card">
                         <CardMedia
                         component="img"
-                        height="140"
-                        image="/static/images/cards/contemplative-reptile.jpg"
+                        image={portfolioProject}
                         alt="green iguana"
+                        className="cardRow2"
                         />
                         <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">Portfolio</Typography>
-                            <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
+                            <Typography gutterBottom variant="h5" component="div">Portfolio Website</Typography>
+                            <Typography variant="body1" color="black">
+                            My personal portfolio website. Built using React.JS, Material.UI, React Router and React Router Hash Link. Fully Responsive.
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
+                            <Box mr={1}>
+                                <Button size="small" onClick={handleOpen} variant="contained" color="success">Full Description</Button>
+                            </Box>
+                            <Button size="small" variant="outlined" href="https://github.com/lewispotticary/portfolio" target="_blank">Source</Button>
+                        </CardActions>
+                    </Grid>
+                    <Grid item component={Card} xs={12} md={3.5} className="card">
+                        <CardMedia
+                        component="img"
+                        image={githubImage}
+                        alt="green iguana"
+                        className="cardRow2"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">Other Projects</Typography>
+                            <Typography variant="body1" color="black" marginBottom="auto">
+                            Take a look at my personal GitHub account where I store all my personal projects, also see what I'm currently working on.
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" variant="contained" href="https://github.com/lewispotticary" target="_blank">All Projects</Button>
                         </CardActions>
                     </Grid>
                 </Grid>
