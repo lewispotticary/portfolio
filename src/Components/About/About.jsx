997886ad0e62579526c2df039e672f3c740f1e28
Lewis Potticary <lewispotticary@gmail.com>
2021-11-09 22:25:09 +0000

    Created about component

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import {Typography, Input, Button} from '@mui/material';
+
+import './About.css';
+
+function About() {
+    return (
+        <div className="aboutContainer" id="About">
+            <div className="aboutContent">
+                <Typography variant="h2" color="white"><b>About</b></Typography>  
+                <hr></hr>
+                <Typography variant="h6" color="white"><b><p>Hi I'm Lewis. I'm aspiring web developer from Broadstairs, Kent. Looking to turn my hobby of creating 
+                web applications into a career. I'm a self taught developer learning front end technologies such as React, JavaScript, HTML and CSS. Looking to secure my 
+                first developer role to put my skills and knowledge on web technologies and tools I have built up throught self learning into practice. If you are interested 
+                in what see, feel free to contact me though the contact form below.
+                
+                </p>
+                    
+                </b></Typography> 
+                <hr></hr>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
