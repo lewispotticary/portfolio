ce798ffe2c4ece53ea8573c5b138345458bf6dcd
Lewis Potticary <lewispotticary@gmail.com>
2021-11-10 22:43:35 +0000

    Added animation to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,22 +8,39 @@ import { HashLink } from 'react-router-hash-link';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import { motion } from 'framer-motion';
+
 function Header() {
     return (
         <BrowserRouter>
             <div className="headerContainer">
                 <div className="headerContent">
-                    <Typography variant="h2" color="white"><b>Hi! I'm Lewis Potticary</b></Typography>
-                    <div className="box">
-                        <Typography variant="h4" color="white"><b>Aspiring Front-end Developer</b></Typography>
-                        <Typography className="boxText" variant="h4" color="white"><b>&nbsp;and Surfer &#10132;</b></Typography>
-                    </div>
-                    <div className="secondaryBox">
-                        <Typography color="white">Picture By: <a href="https://www.elliscollinsphoto.co.uk/" target="_blank">Ellis Collins</a></Typography>
-                    </div>
-                    <HashLink smooth className="navLinks" to="#Projects">
-                        <Button className="projectButton" variant="contained">View Projects</Button>
-                    </HashLink>
+                    <motion.div
+                        animate={{ x: [-1000, 100, 0] }}
+                        transition={{ ease: "easeOut", duration: 1 }}
+                    >
+                        <Typography variant="h2" color="white"><b>Hi! I'm Lewis Potticary</b></Typography>
+                    </motion.div>
+                    <motion.div 
+                        animate={{ x: [-1000, 100, 0] }}
+                        transition={{ ease: "easeOut", duration: 1, delay: 1 }}
+                    >
+                        <div className="box">
+                            <Typography variant="h4" color="white"><b>Aspiring Front-end Developer</b></Typography>
+                            <Typography className="boxText" variant="h4" color="white"><b>&nbsp;and Surfer &#10132;</b></Typography>
+                        </div>
+                        <div className="secondaryBox">
+                            <Typography color="white">Picture By: <a href="https://www.elliscollinsphoto.co.uk/" target="_blank">Ellis Collins</a></Typography>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        animate={{ x: [-1000, 100, 0] }}
+                        transition={{ ease: "easeOut", duration: 1, delay: 1.5 }}
+                    >
+                        <HashLink smooth className="navLinks" to="#Projects">
+                            <Button className="projectButton" variant="contained">View Projects</Button>
+                        </HashLink>   
+                    </motion.div>
                 </div>
             </div>
         </BrowserRouter>
