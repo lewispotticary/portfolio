c1ee1c5654b5f78dbc5e5522e0096edddfec8700
Lewis Potticary <lewispotticary@gmail.com>
2021-11-13 17:11:49 +0000

    Added background colour to text

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,11 +13,13 @@ function About() {
                 <Typography className="test" variant="h2" color="white"><b>About.</b></Typography>  
                 <Animation>
                 <hr></hr>
+                <div className="aboutBox">
                 <Typography variant="h6" color="white"><b><p>Hi I'm Lewis. I'm aspiring web developer from Broadstairs, Kent. Looking to turn my hobby of creating 
                 web applications into a career. I'm a self taught developer learning front end technologies such as React, JavaScript, HTML and CSS. Looking to secure my 
                 first developer role to put my skills and knowledge on web technologies and tools I have built up throught self learning into practice. If you are interested 
                 in what see, feel free to contact me though the contact form below.
                 </p></b></Typography> 
+                </div>
                 <hr></hr>
                 </Animation>
             </div>
